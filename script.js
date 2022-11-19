575d2826a2f9810be50ba7bcf871505ead538bb9
Moritz Meyer <moritzmaximilian@gmx.net>
2022-11-19 22:46:25 +0100

    .

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
-function reveal() {
-    var reveal = document.querySelectorAll(".reveal");
+function fadeShort() {
+    var fadeShort = document.querySelector(".fadeShort");
+    
+    setTimeout(function() {
+        fadeShort.classList.add("active");
+    }, 500);
+}
 
-    for (var i = 0; i < reveal.length; i++) {
+function fadeIntro() {
+    var fadeIntro = document.querySelector(".fadeIntro");
+
+    setTimeout(function() {
+        fadeIntro.classList.add("active");
+    }, 1000);
+}
+
+function fadeRight() {
+    var fadeRight = document.querySelectorAll(".fadeRight");
+
+    for (var i = 0; i < fadeRight.length; i++) {
+
         var windowHeight = window.innerHeight;
-        var elementTop = reveal[i].getBoundingClientRect().top;
+        var elementTop = fadeRight[i].getBoundingClientRect().top;
         var elementVisible = 200;
+
         if (elementTop < windowHeight - elementVisible) {
-            reveal[i].classList.add("active");
+            fadeRight[i].classList.add("active");
         } else {
-            reveal[i].classList.remove("active");
+            fadeRight[i].classList.remove("active");
         }
     }
 }
 
-window.addEventListener("scroll", reveal);
+function fadeLeft() {
+    var fadeLeft = document.querySelectorAll(".fadeLeft");
+
+    for (var i = 0; i < fadeLeft.length; i++) {
+
+        var windowHeight = window.innerHeight;
+        var elementTop = fadeLeft[i].getBoundingClientRect().top;
+        var elementVisible = 200;
+        
+        if (elementTop < windowHeight - elementVisible) {
+            fadeLeft[i].classList.add("active");
+        } else {
+            fadeLeft[i].classList.remove("active");
+        }
+    }
+}
+
+function openSoundCloud() {
+    window.open("https://soundcloud.com/mrtzmxmln");
+}
+
+function openInstagram() {
+    window.open("https://www.instagram.com");
+}
+
+function openPiknic() {
+    window.open("http://mrtzmxmln.github.io/risingufo");
+}
+
+window.addEventListener("scroll", fadeRight);
+window.addEventListener("scroll", fadeLeft);
+fadeShort();
+fadeIntro();
